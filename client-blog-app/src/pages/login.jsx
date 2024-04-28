45c5b6b2c4bf8845c7be48909375a1e505252af5
vijayshiv <vijay.arya1213@gmail.com>
2024-04-28 19:33:53 +0530

    Registration Page Started

---
+++ client-blog-app/src/pages/login.jsx
@@ -0,0 +1,19 @@
+export default function Login() {
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: "greenyellow",
+          color: "#0066b2",
+          textAlign: "center",
+          fontSize: 70,
+          fontFamily: "sans-serif",
+          fontWeight: "bold",
+        }}
+      >
+        Registration Here
+      </div>
+      <div className="form"></div>
+    </div>
+  );
+}
