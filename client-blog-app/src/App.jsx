9953f60b0739ff338e5a208d3564288514e49717
vijayshiv <vijay.arya1213@gmail.com>
2024-04-28 19:33:53 +0530

    Registration Page Started

---
+++ client-blog-app/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
