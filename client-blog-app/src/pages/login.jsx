aa6de4798d90570b03e4acf16f055a6fcca19279
vijayshiv <vijay.arya1213@gmail.com>
2024-04-30 00:11:18 +0530

    Registration form practice UI with css only bohot basic hai

--- client-blog-app/src/pages/login.jsx
+++ client-blog-app/src/pages/login.jsx
@@ -1,19 +1,92 @@
+import React from "react";
+import sideImage from "../images/side.jpg"; // Import the image
+
 export default function Login() {
   return (
-    <div>
+    <section className="bgimage">
       <div
         style={{
-          backgroundColor: "greenyellow",
-          color: "#0066b2",
-          textAlign: "center",
-          fontSize: 70,
-          fontFamily: "sans-serif",
-          fontWeight: "bold",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
         }}
       >
-        Registration Here
+        <div>
+          <img
+            src={sideImage}
+            alt="Image"
+            style={{
+              borderTopLeftRadius: 10,
+              borderBottomLeftRadius: 10,
+              backgroundColor: "white",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: 500,
+              width: 400,
+            }}
+          />{" "}
+          {/* Use the imported image */}
+        </div>
+        <div
+          className="form"
+          style={{
+            borderTopRightRadius: 10,
+            borderBottomRightRadius: 10,
+            backgroundColor: "#8CBED6",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: 500,
+            width: 400,
+          }}
+        >
+          <div style={{ paddingBottom: 15, fontSize: 25, fontWeight: 650 }}>
+            Registration Here
+          </div>
+          <div>
+            <label htmlFor="">Name : </label>
+            <br />
+            <input type="text" style={{ height: 30, width: 270 }} />
+          </div>
+          <div>
+            <label htmlFor="">Email : </label>
+            <br />
+            <input type="text" style={{ height: 30, width: 270 }} />
+          </div>
+          <div>
+            <label htmlFor="">Password : </label>
+            <br />
+            <input type="text" style={{ height: 30, width: 270 }} />
+          </div>
+          <div>
+            <label htmlFor="">Confirm Password : </label>
+            <br />
+            <input type="text" style={{ height: 30, width: 270 }} />
+          </div>
+          <div>
+            <label htmlFor="">Phone Number : </label>
+            <br />
+            <input type="text" style={{ height: 30, width: 270 }} />
+          </div>{" "}
+          <div>
+            <label htmlFor="">Address : </label>
+            <br />
+            <input type="text" style={{ height: 30, width: 270 }} />
+          </div>
+          <div>
+            <label htmlFor="">Adhaar Number : </label>
+            <br />
+            <input type="text" style={{ height: 30, width: 270 }} />
+          </div>
+          <button type="submit" className="btn btn-primary mt-3">
+            Submit
+          </button>
+        </div>
       </div>
-      <div className="form"></div>
-    </div>
+    </section>
   );
 }
