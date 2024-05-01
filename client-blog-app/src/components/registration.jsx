d2b227fa641ccfeeba7d65d4a5b34469be243801
vijayshiv <vijay.arya1213@gmail.com>
2024-05-01 18:48:13 +0530

    From Completed

--- client-blog-app/src/components/registration.jsx
+++ client-blog-app/src/components/registration.jsx
@@ -1,51 +1,125 @@
 import email from "../components/assets/email.jpeg";
-import name from "../components/assets/name.png";
-import password from "../components/assets/password.png";
-import adhaar from "../components/assets/adhaar.png";
-import phone from "../components/assets/phone.png";
 
 export default function Register() {
   return (
     <>
-      <div className="container">
-        <div className="header">
-          <div className="text">Registration</div>
-          <div className="underline"></div>
-        </div>
-        <div className="inputs">
-          <div className="input">
-            <img src={name} alt="" height={14} width={12} />
-            <label htmlFor="">Name</label>
-            <input type="text" />
-          </div>
-          <div className="input">
-            <img src={email} alt="" height={14} width={12} />
-            <label htmlFor="">Email</label>
-            <input type="text" />
-          </div>
-          <div className="input">
-            <img src={password} alt="" height={14} width={12} />
-            <label htmlFor="">Password</label>
-            <input type="text" />
-          </div>
-          <div className="input">
-            <img src={password} alt="" height={14} width={12} />
-            <label htmlFor="">Confirm Password</label>
-            <input type="text" />
-          </div>
-          <div className="input">
-            <img src={phone} alt="" height={14} width={12} />
-            <label htmlFor="">Phone Number</label>
-            <input type="text" />
-          </div>
-          <div className="input">
-            <img src={adhaar} alt="" height={14} width={12} />
-            <label htmlFor="">Aadhaar Number</label>
-            <input type="text" />
-          </div>
-          <div className="submit-container">
-            <div className="submit">Sign Up</div>
-          </div>
+      <div
+        className="dark-overlay"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.2)", // Dark overlay color with 50% opacity
+        }}
+      ></div>
+      <div
+        className="container-fluid d-flex justify-content-center align-items-center"
+        style={{ height: "100vh", background: "skyblue" }}
+      >
+        <div
+          className="login-template"
+          style={{
+            background: "linear-gradient(to right, #aee1f9, #f6ebe6)",
+            borderRadius: "10px",
+            boxShadow: "0 4px 6px rgba(0,0,0,0.4)",
+            width: "30%",
+            padding: "50px",
+            paddingBottom: "30px",
+            paddingTop: "30px",
+          }}
+        >
+          <form>
+            <h2
+              style={{
+                marginBottom: "20px",
+                color: "#333",
+                textAlign: "center",
+              }}
+            >
+              Sign Up
+            </h2>
+            <div className="row g-3">
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="First name"
+                  aria-label="First name"
+                />
+              </div>
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Last name"
+                  aria-label="Last name"
+                />
+              </div>
+            </div>
+            <br />
+            <div className="row g-3">
+              <div className="col-md-12">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Email"
+                  aria-label="Email"
+                />
+              </div>
+            </div>
+            <br />
+            <div className="row g-3">
+              <div className="col-md-12">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Password"
+                  aria-label="Password"
+                />
+              </div>
+            </div>
+            <br />
+            <div className="row g-3">
+              <div className="col-md-12">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Confirm Password"
+                  aria-label="Confirm Password"
+                />
+              </div>
+            </div>
+            <br />
+            <div className="row g-3">
+              <div className="col-md-12">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Phone Number"
+                  aria-label="Phone Number"
+                />
+              </div>
+            </div>
+            <br />
+            <div className="row g-3">
+              <div className="col-md-12">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Aadhaar Card Number"
+                  aria-label="Aadhaar Card Number"
+                />
+              </div>
+            </div>
+            <br />
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
