4118f404082b2f4b24739a2e93982e6732b2f72b
D1_Vijayshiv_83724 <vijay.arya1213@gmail.com>
2024-06-16 01:25:24 +0530

    my post CRUD api are done

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -20,7 +20,7 @@ router.get("/all", (req, res) => {
 
 // Create a new post
 router.post("/add-post", upload.single("image"), (req, res) => {
-  // Use req.user.userId to get the user ID
+  // Use req.user.id to get the user ID
   const userId = req.user.id;
 
   try {
@@ -64,6 +64,54 @@ router.get("/by-category/:category", (req, res) => {
       res.send(util.errorMessage(error));
     } else {
       res.send(util.successMessage(data));
+    }
+  });
+});
+
+router.get("/my-all-post", (req, res) => {
+  // getting the user id which is stored in storage
+  const userId = req.user.id;
+  const query = `SELECT post_id, title, content, img, category FROM posts WHERE user_id = ? AND isDeleted = 0;`;
+
+  db.pool.execute(query, [userId], (error, all_post) => {
+    res.send(util.result(all_post, error));
+  });
+});
+
+router.put("/update-post/:postId", (req, res) => {
+  const postId = req.params.postId;
+  const { title, content, img, category } = req.body;
+
+  const query = `UPDATE posts SET title = ?, content = ?, img = ?, category = ? WHERE post_id = ?`;
+  const values = [title, content, img, category, postId];
+
+  db.pool.execute(query, values, (error, result) => {
+    if (error) {
+      res.send(util.errorMessage("Internal server error"));
+    } else {
+      if (result.affectedRows > 0) {
+        res.send(util.errorMessage("Post updated successfully"));
+      } else {
+        res.send(util.errorMessage("Post not found"));
+      }
+    }
+  });
+});
+
+router.delete("/delete-post/:postId", (req, res) => {
+  const postId = req.params.postId;
+
+  const query = `UPDATE posts SET isDeleted = 1 WHERE post_id = ?`;
+
+  db.pool.execute(query, [postId], (error, result) => {
+    if (error) {
+      res.send(util.errorMessage("Internal server error"));
+    } else {
+      if (result.affectedRows > 0) {
+        res.send(util.errorMessage("Post deleted successfully"));
+      } else {
+        res.send(util.errorMessage("Post not found"));
+      }
     }
   });
 });
