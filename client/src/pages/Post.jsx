6bec594f5b0327292fdefe7326c4f3803df114ee
D1_Vijayshiv_83724 <vijay.arya1213@gmail.com>
2024-06-16 13:41:42 +0530

    New component for suggestions and some changes so that the data gets rendered properly

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Suggestions from "../components/Suggestions";
 
 const Post = () => {
   const { id } = useParams();
@@ -81,7 +82,7 @@ const Post = () => {
       <div className="container mx-auto mt-16">
         <div className="flex">
           <div className="w-3/4">
-            <h1 className="font-bold text-4xl lg:text-6xl text-blue-900 mb-8 leading-tight font-serif">
+            <h1 className="font-bold text-3xl lg:text-5xl text-blue-900 mb-8 leading-tight font-serif">
               "{post.title}"
             </h1>
             <div className="clearfix">
@@ -92,8 +93,11 @@ const Post = () => {
                 onClick={() => navigate(`/post/${post.post_id}`)}
               />
               <div
-                className=" text-justify mt-4"
-                style={{ fontSize: "120%" }}
+                className="mt-4"
+                style={{
+                  fontSize: "130%",
+                  textAlign: "justify",
+                }}
                 dangerouslySetInnerHTML={{ __html: post.content }}
               />
             </div>
@@ -105,41 +109,7 @@ const Post = () => {
               </p>
             </div>
           </div>
-          <div className="w-1/4 h- overflow-y-auto">
-            <div className="mt-3 ml-12">
-              <h2 className="font-bold text-2xl mb-4">
-                Other Suggested Posts You May Like
-              </h2>
-              <ul>
-                {suggestions.map((item) => (
-                  <li key={item.post_id} className="mb-4">
-                    <Link
-                      to={`/post/${item.post_id}`}
-                      className="block"
-                      onClick={() => window.scrollTo(0, 0)}
-                    >
-                      <img
-                        src={`http://localhost:4000/images/${item.img}`}
-                        alt={item.title}
-                        className="w-full h-56 object-cover rounded-md mb-2 cursor-pointer"
-                      />
-                      <h3 className="text-2xl mb-4 font-semibold text-blue-700 hover:underline cursor-pointer text-left">
-                        {item.title}
-                      </h3>
-                    </Link>
-                    <p className="text-gray-600">{item.category}</p>
-                    <Link
-                      to={`/post/${item.post_id}`}
-                      className="text-black-700 hover:underline bg-slate-200 hover:bg-slate-300 p-1 rounded-sm"
-                      onClick={() => window.scrollTo(0, 0)}
-                    >
-                      Read More
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
+          <Suggestions suggestions={suggestions} />
         </div>
       </div>
     </>
