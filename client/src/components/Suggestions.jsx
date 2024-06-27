cbdd5f092d6f7f251e179a3dc39d6ccfce84262e
vijayshiv <vijay.arya1213@gmail.com>
2024-06-27 17:37:47 +0530

    suggestion bar responsive

--- client/src/components/Suggestions.jsx
+++ client/src/components/Suggestions.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Suggestions = ({ suggestions }) => {
   return (
-    <div className="overflow-x-auto lg:overflow-y-auto">
-      <div className="mt-3 lg:ml-12">
-        <h2 className="font-bold text-2xl mb-4">
-          Other Suggested Posts You May Like
-        </h2>
+    <div className="lg:ml-12">
+      <h2 className="font-bold text-2xl mb-4">
+        Other Suggested Posts You May Like
+      </h2>
+      <div className="overflow-x-auto lg:overflow-y-auto">
         <ul
           className="flex lg:block space-x-4 lg:space-x-0"
           style={{ listStyleType: "none" }}
@@ -24,7 +24,7 @@ const Suggestions = ({ suggestions }) => {
                   alt={item.title}
                   className="w-40 h-40 lg:w-full lg:h-56 object-cover rounded-md mb-2 cursor-pointer"
                 />
-                <h3 className="text-md text-center lg:text-2xl mb-4 font-semibold text-blue-700 hover:underline cursor-pointer ">
+                <h3 className="text-md text-center lg:text-2xl mb-4 font-semibold text-blue-700 hover:underline cursor-pointer truncate-text">
                   {item.title}
                 </h3>
               </Link>
@@ -40,6 +40,18 @@ const Suggestions = ({ suggestions }) => {
           ))}
         </ul>
       </div>
+      <style>
+        {`
+          @media (max-width: 768px) {
+            .truncate-text {
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+              max-width: 150px; /* Adjust this width as needed */
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
