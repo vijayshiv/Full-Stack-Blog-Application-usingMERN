fd345c6e2deb0ed4a6386463e30d14e7513600ed
vijayshiv <vijay.arya1213@gmail.com>
2025-07-24 15:04:14 +0530

    Notification service is working but not tested yet

--- server/dist/controllers/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { UserController } from './userController';
-export { PostController } from './postController';
-export { CommentController } from './commentController';
-export { default as UserControllerDefault } from './userController';
-export { default as PostControllerDefault } from './postController';
-export { default as CommentControllerDefault } from './commentController';
-//# sourceMappingURL=index.d.ts.map
--- server/dist/services/index.d.ts
+++
@@ -1,9 +0,0 @@
-export { UserService } from "./userService";
-export { PostService } from "./postService";
-export { CommentService } from "./commentService";
-export { NotificationService } from "./notificationService";
-export { default as UserServiceDefault } from "./userService";
-export { default as PostServiceDefault } from "./postService";
-export { default as CommentServiceDefault } from "./commentService";
-export { default as NotificationServiceDefault } from "./notificationService";
-//# sourceMappingURL=index.d.ts.map
